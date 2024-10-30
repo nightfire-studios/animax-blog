228b08854a9586facc8a33a463c936989afa56b0
Terah <terahquarteynk@gmail.com>
2024-10-30 16:31:11 -0700

    Project Initialzed

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom"
+import list from "./images/list.png"
+import menu from"./images/menu.png"
 
 const Dashboard = ()=>{
     return (
         <>
             <Link to="/">&larr; Back</Link>
+            <nav>
+                <div className="container">
+                    <div className="main_heading">
+                        <p>Recently Published</p>
+                    </div>
+                    <div className="corner_items">
+                        <div className="rows">
+                            <img src={list} alt="checklist" />
+                        </div>
+                        <div className="grid">
+                            <img src={menu} alt="menu" />
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
         </>
     )
 }
